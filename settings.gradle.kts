f86c0ab31f71dbf64873538adfe9de140ebe0139
Yurii Popiv <yurii@Yuriis-MacBook-Pro.local>
2024-06-21 20:08:20 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "newsapp"
+include(":app")
+include(":news-api")
+include(":database")
+include(":news-main")
+include(":news-data")
+include(":news-common")
